9367540e610bf86e9ce035cc7f5b972c0226a315
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-09-16 13:36:43 +0530

    'hiii'

--- JavaPrograms/src/Assessment/Array4.java
+++ JavaPrograms/src/Assessment/Array4.java
@@ -1,28 +1,34 @@
-//Example 1:
-//Input: N = 5, arr[] = {5,4,3,2,1}
-//Output: {1,2,3,4,5}
+//Example 2:
+//Input: N=6 arr[] = {10,20,30,40}
+//Output: {40,30,20,10}
 //Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+
+//Using Space-optimized iterative mtd
 
 package Assessment;
 
 import java.util.Arrays;
 
-public class Array3 {
+public class Array4 {
 	//Function to print array
-	static void printArray(int ans[], int n) {
-		System.out.println("Reversed Array is: \n");
+	static void printArray(int arr[], int n) {
+		System.out.println("Reversed array is: \n");
 		for (int i=0; i<n; i++) {
-			System.out.println(ans[i]+" ");
+			System.out.println(arr[i]+" ");
 		}
 	}
 	
-	//Function to reverse array using an auxiliary array
+	//Function to reverse array
 	static void reverseArray(int arr[], int n) {
-		int[] ans =new int[n];
-		for (int i=n-1; i>=0; i--) {
-			ans[n- i- 1] =arr[i];
+		int p1=0, p2=n-1;
+		while(p1<p2) {
+		int tmp =arr[p1];
+		arr[p1]=arr[p2];
+		arr[p2]=tmp;
+		p1++;
+		p2--;
 		}
-		printArray(ans, n);
+		printArray(arr, n);
 	}
 	public static void main(String[] args) {
 		int n=5;
